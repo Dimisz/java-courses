17b6021c36342b975cf51297a94844d83fc6dc62
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-15 20:50:37 +0800

    Variables basics

---
+++ slim/variables/GryffindorScore.java
@@ -0,0 +1,12 @@
+public class GryffindorScore {
+  public static void main(String[] args) {
+    int gryffindorScore = 0;
+    gryffindorScore -= 50;
+    gryffindorScore -= 3;
+    gryffindorScore += 30;
+    gryffindorScore += 100;
+    gryffindorScore += 60;
+    System.out.println(gryffindorScore);
+
+  }
+}
---
+++ slim/variables/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+  public static void main(String[] args) {
+    int passengers = 4;
+    System.out.println(passengers);
+    passengers = passengers + 10;
+    System.out.println(passengers);
+  }
+}
