8b86d44b4d6cf738ae19ab7b52c0fb3020fd9919
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-11 17:12:01 +0800

    Strings and chars in Java

---
+++ slim/variables/StringsAndChars.java
@@ -0,0 +1,9 @@
+public class StringsAndChars {
+  public static void main(String[] args) {
+    int year = 2022;
+    String name = "John Doe";
+    char grade = 'A';
+
+    System.out.println(name + " graduated in " + year + " with a grade " + grade + ".");
+  }
+}
