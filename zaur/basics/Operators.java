668e0e9aff352c9b5c5e730b1af8f8a4f9fa1a56
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-31 20:58:56 +0800

    Housekeeping

---
+++ zaur/basics/Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+  public static void main(String[] args) {
+    int x = 1, y = 2, z = 3;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    System.out.println("First argument: " + args[0]);
+  }
+}
