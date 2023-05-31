1ec4ba7feb9b5f39711c571b1cb8b57cdf4b9092
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-31 17:21:33 +0800

    Housekeeping

---
+++ zaur/basics/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+  public static void main(String[] args) {
+    int year = 2010;
+    String winner = "Spain";
+
+    System.out.println("The winner of the " + year + " World cup is " + winner + ".");
+  }
+}
