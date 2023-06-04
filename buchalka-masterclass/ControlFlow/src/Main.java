ea5573c3abd43a57c3139a70f1934b2f062d75fe
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-04 15:27:22 +0800

    for loops

--- buchalka-masterclass/ControlFlow/src/Main.java
+++ buchalka-masterclass/ControlFlow/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("0: error " + PrimeNumberChecker.isPrime(0));
-        System.out.println("1: error " + PrimeNumberChecker.isPrime(1));
-        System.out.println("3: true " + PrimeNumberChecker.isPrime(3));
-        System.out.println("5: true " + PrimeNumberChecker.isPrime(5));
-        System.out.println("6: false " + PrimeNumberChecker.isPrime(6));
+        System.out.println(PrimeNumberChecker.primeNumbersInRange(10, 50));
     }
 }
