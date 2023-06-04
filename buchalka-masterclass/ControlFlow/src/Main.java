413ac8eca1c87cbc362197ab14d7e8e8af69717a
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-04 09:36:37 +0800

    New and old style switch statements

---
+++ buchalka-masterclass/ControlFlow/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("A is spelled as: " + NatoAlphabet.getNatoSpelling('A'));
+//        System.out.println("B is spelled as: " + NatoAlphabet.getNatoSpelling('B'));
+//        System.out.println("C is spelled as: " + NatoAlphabet.getNatoSpelling('C'));
+//        System.out.println("a is spelled as: " + NatoAlphabet.getNatoSpelling('a'));
+//        System.out.println("b is spelled as: " + NatoAlphabet.getNatoSpelling('b'));
+//        System.out.println("c is spelled as: " + NatoAlphabet.getNatoSpelling('c'));
+//        System.out.println("d is spelled as: " + NatoAlphabet.getNatoSpelling('d'));
+//        System.out.println("E is spelled as: " + NatoAlphabet.getNatoSpelling('E'));
+
+        System.out.println(NatoAlphabet.printDayOfWeek(1));
+        System.out.println(NatoAlphabet.printDayOfWeek(2));
+        System.out.println(NatoAlphabet.printDayOfWeek(3));
+        System.out.println(NatoAlphabet.printDayOfWeek(4));
+        System.out.println(NatoAlphabet.printDayOfWeek(5));
+        System.out.println(NatoAlphabet.printDayOfWeek(6));
+        System.out.println(NatoAlphabet.printDayOfWeek(7));
+        System.out.println(NatoAlphabet.printDayOfWeek(-1));
+        System.out.println(NatoAlphabet.printDayOfWeek(17));
+    }
+}
