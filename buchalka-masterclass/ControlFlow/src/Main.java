cfc126fb0f74fc69352abc16fbda3bc689caa53c
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-08 09:21:10 +0800

    More examples with Scanner

--- buchalka-masterclass/ControlFlow/src/Main.java
+++ buchalka-masterclass/ControlFlow/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LargestPrimeFactor.getLargestPrime(21));
-        System.out.println(LargestPrimeFactor.getLargestPrime(217));
-        System.out.println(LargestPrimeFactor.getLargestPrime(0));
-        System.out.println(LargestPrimeFactor.getLargestPrime(45));
-        System.out.println(LargestPrimeFactor.getLargestPrime(-1));
+       InputCalculator.inputThenPrintSumAndAverage();
     }
 }
