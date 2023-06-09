63402e0260d5668d8fe20f47f7d786a964a62062
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-09 08:45:37 +0800

    Start dice game

---
+++ cs1331/DiceProject/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Die die1 = new Die();
+        System.out.println(die1);
+        System.out.println(die1.roll());
+        System.out.println(die1.roll());
+    }
+}
