865e758a97073bf90a23717d62f3909fe38e6e52
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-13 19:12:58 +0800

    Wall class

--- buchalka-masterclass/OOP/src/Main.java
+++ buchalka-masterclass/OOP/src/Main.java
@@ -1,19 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i <= 5; i++){
-            LPAStudent s = new LPAStudent("S92300" + i,
-                    switch(i){
-                        case 1 -> "Mary";
-                        case 2 -> "Carol";
-                        case 3 -> "Tim";
-                        case 4 -> "Harry";
-                        case 5 -> "Lisa";
-                        default -> "Anonymous";
-                    },
-                    "05/11/1985",
-                    "Java Masterclass"
-                    );
-            System.out.println(s);
-        }
+//        for(int i = 0; i <= 5; i++){
+//            LPAStudent s = new LPAStudent("S92300" + i,
+//                    switch(i){
+//                        case 1 -> "Mary";
+//                        case 2 -> "Carol";
+//                        case 3 -> "Tim";
+//                        case 4 -> "Harry";
+//                        case 5 -> "Lisa";
+//                        default -> "Anonymous";
+//                    },
+//                    "05/11/1985",
+//                    "Java Masterclass"
+//                    );
+//            System.out.println(s);
+//        }
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+
+
     }
 }
