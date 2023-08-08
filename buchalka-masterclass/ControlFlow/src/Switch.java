1235e6cb9caa70ffbcc6e386337c528f11b385cb
Vladimir Solovyov <leilyngale@icloud.com>
2023-08-08 10:51:54 +0800

    Replaced with the enhanced switch

--- buchalka-masterclass/ControlFlow/src/Switch.java
+++ buchalka-masterclass/ControlFlow/src/Switch.java
@@ -2,19 +2,11 @@ public class Switch {
     public static void main(String[] args) {
         int switchValue = 10;
 
-        switch(switchValue){
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
-                System.out.println("It was 3 or 4 or 5.");
-                break;
-            default:
-                System.out.println("It was something else.");
-                break;
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> System.out.println("It was 3 or 4 or 5.");
+            default -> System.out.println("It was something else.");
         }
     }
 }
