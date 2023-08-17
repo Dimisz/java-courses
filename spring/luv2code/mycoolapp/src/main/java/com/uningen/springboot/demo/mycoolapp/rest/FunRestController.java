42530ac84a38c34b2cab826312ac751cc91461bb
Vladimir Solovyov <leilyngale@icloud.com>
2023-08-17 23:37:09 +0800

    First spring boot app

--- spring/luv2code/mycoolapp/src/main/java/com/uningen/springboot/demo/mycoolapp/rest/FunRestController.java
+++ spring/luv2code/mycoolapp/src/main/java/com/uningen/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,19 +1,37 @@
 package com.uningen.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-
-    // expose a "/" endpoint that returns "hello world"
+    // injecting properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
     @GetMapping("/")
     public String sayHello(){
-        return "Hello world!";
+        return "Hello world! How are you?";
+    }
+    @GetMapping("/bye")
+    public String sayBye() {
+        return "Good bye!!!!";
     }
 
-    @GetMapping("/about")
-    public String aboutPage(){
-        return "<h1>This is a home page</h1>";
+    @GetMapping("/team/coach")
+    public String getCoachName(){
+        return "Coach name is: " + this.coachName;
+    }
+
+    @GetMapping("/team/name")
+    public String getTeamName(){
+        return "The team name is " + this.teamName;
+    }
+
+    @GetMapping("/workin")
+    public String dailyWorkout(){
+        return "Run a hard 5k";
     }
 }
