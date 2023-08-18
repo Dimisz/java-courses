3b33225d25312fb9579030228ba5e6edbae0b456
Vladimir Solovyov <leilyngale@icloud.com>
2023-08-18 23:38:15 +0800

    Housekeeping

--- buchalka-masterclass/ControlFlow/src/Main.java
+++ buchalka-masterclass/ControlFlow/src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(NumberOfDaysInMonth.isLeapYear(-1600));
-        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
-        System.out.println(NumberOfDaysInMonth.isLeapYear(2017));
-        System.out.println(NumberOfDaysInMonth.isLeapYear(2000));
-
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2020));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2020));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2020));
+        PrimeNumber.countPrimes(10, 50);
     }
 }
---
+++ buchalka-masterclass/ControlFlow/src/PrimeNumber.java
@@ -0,0 +1,38 @@
+public class PrimeNumber {
+    private static boolean isPrime(int num){
+       if(num <= 2){
+           return (num == 2);
+       }
+        else{
+            for(int i = 2; i <= num / 2; i++){
+                if(num % i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void printIsPrime(int num){
+        System.out.println(num + " is " + (isPrime(num) ? "" : "NOT") + " a prime number");
+    }
+
+    public static void countPrimes(int lowerBound, int upperBound){
+        if(lowerBound < 0 || upperBound > 1000){
+            System.out.println("Should be a positive number from 1 to 1000");
+        }
+        else if(lowerBound > upperBound){
+            System.out.println("Lower bound should be smaller than the upper bound.");
+        }
+        else {
+            int primesCount = 0;
+            for(int i = lowerBound; i < upperBound; i++){
+                if(isPrime(i)){
+                    primesCount++;
+                    System.out.println(i);
+                }
+                if(primesCount == 3) break;
+            }
+        }
+    }
+}
