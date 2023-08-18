2f5263687f66293cd162c0024604ec72799e93dd
Vladimir Solovyov <leilyngale@icloud.com>
2023-08-18 22:49:35 +0800

    More practice on control flow

--- buchalka-masterclass/ControlFlow/src/Main.java
+++ buchalka-masterclass/ControlFlow/src/Main.java
@@ -1,24 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String day = SwitchExpressionChallenge.getDayOfWeek(0);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(1);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(2);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(3);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(4);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(5);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(6);
-        System.out.println(day);
+        System.out.println(NumberOfDaysInMonth.isLeapYear(-1600));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2017));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2000));
 
-        System.out.println("Checking with invalid args");
-        day = SwitchExpressionChallenge.getDayOfWeek(-1);
-        System.out.println(day);
-        day = SwitchExpressionChallenge.getDayOfWeek(10);
-        System.out.println(day);
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2020));
     }
 }
