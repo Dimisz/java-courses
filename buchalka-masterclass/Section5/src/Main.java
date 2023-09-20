c9f21db0ca5511dfb15046ad50c2dd04a533511e
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-20 23:21:31 +0800

    Numbers challenge

---
+++ buchalka-masterclass/Section5/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PositiveNegativeOrZero.checkNumber(-1);
+        PositiveNegativeOrZero.checkNumber(0);
+        PositiveNegativeOrZero.checkNumber(1);
+    }
+}
