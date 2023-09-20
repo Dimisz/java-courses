eebf29babb9396a2310b290b6ea553ab4529de22
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-20 23:30:40 +0800

    Speed converter

--- buchalka-masterclass/Section5/src/Main.java
+++ buchalka-masterclass/Section5/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        PositiveNegativeOrZero.checkNumber(-1);
-        PositiveNegativeOrZero.checkNumber(0);
-        PositiveNegativeOrZero.checkNumber(1);
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(10.25);
+        SpeedConverter.printConversion(-5.6);
+        SpeedConverter.printConversion(25.42);
+        SpeedConverter.printConversion(75.114);
     }
 }
