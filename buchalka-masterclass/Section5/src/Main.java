4c2f5aeabc11761a63119038fda57b5d456b3ca0
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-21 21:26:52 +0800

    Two more challenges

--- buchalka-masterclass/Section5/src/Main.java
+++ buchalka-masterclass/Section5/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(TeenNumberChecker.hasTeen(9, 99, 19));
+        System.out.println(TeenNumberChecker.hasTeen(23, 15, 42));
+        System.out.println(TeenNumberChecker.hasTeen(22, 23, 34));
     }
 }
