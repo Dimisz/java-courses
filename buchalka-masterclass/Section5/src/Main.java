b9b46afa44680a10b69265176235fcc8c2351624
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-21 20:49:18 +0800

    Leap year calculator

--- buchalka-masterclass/Section5/src/Main.java
+++ buchalka-masterclass/Section5/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.print("false -> ");
+        System.out.println(LeapYearCalculator.isLeapYear(-1600));
         System.out.print("true -> ");
-        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+        System.out.println(LeapYearCalculator.isLeapYear(1600));
         System.out.print("false -> ");
-        System.out.println(BarkingDog.shouldWakeUp(false, 2));
-        System.out.print("false -> ");
-        System.out.println(BarkingDog.shouldWakeUp(true, 8));
-        System.out.print("false -> ");
-        System.out.println(BarkingDog.shouldWakeUp(true, -1));
+        System.out.println(LeapYearCalculator.isLeapYear(2017));
+        System.out.print("true -> ");
+        System.out.println(LeapYearCalculator.isLeapYear(2000));
     }
 }
