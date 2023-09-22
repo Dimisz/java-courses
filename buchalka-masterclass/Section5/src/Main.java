9a67051595148fd4b2010560f20d4cc0416f73df
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-22 22:26:03 +0800

    Playing cat

--- buchalka-masterclass/Section5/src/Main.java
+++ buchalka-masterclass/Section5/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       IntEqualityPrinter.printEqual(1, 1, 1);
-        IntEqualityPrinter.printEqual(1, 1, 2);
-        IntEqualityPrinter.printEqual(-1, -1, -1);
-        IntEqualityPrinter.printEqual(1, 2, 3);
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
     }
 }
