801ebc7b2c72dd8c5b230d5b2596390afe2a2ea9
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-23 08:48:00 +0800

    Num days in a month challenge

--- buchalka-masterclass/Section5/src/LeapYearCalculator.java
+++ buchalka-masterclass/Section5/src/LeapYearCalculator.java
@@ -1,13 +1,11 @@
 public class LeapYearCalculator {
-    public static boolean isLeapYear(int year){
-        if(year < 1 || year > 9999) return false;
-        if(year % 400 == 0){
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) return false;
+        if (year % 400 == 0) {
             return true;
-        }
-        else if(year % 4 == 0 && year % 100 != 0){
+        } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
