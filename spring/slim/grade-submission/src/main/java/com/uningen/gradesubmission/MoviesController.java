9639049dd8de4c61510fcbf90b4661758f47ef71
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-25 21:33:20 +0800

    Modern approach using Model class

--- spring/slim/grade-submission/src/main/java/com/uningen/gradesubmission/MoviesController.java
+++ spring/slim/grade-submission/src/main/java/com/uningen/gradesubmission/MoviesController.java
@@ -12,22 +12,25 @@ import org.springframework.web.servlet.ModelAndView;
 public class MoviesController {
 
   @GetMapping("/movies")
-  public ModelAndView getMovies() {
-    // model.addAttribute("first", new Movie("Breaking Bad", "Ozymandias", 10.0));
-    // model.addAttribute("second", new Movie("Attack on Titan", "Hero", 9.9));
-    // model.addAttribute("third", new Movie("Attack on Titan", "Perfect Game",
-    // 9.9));
-    // model.addAttribute("fourth", new Movie("Star Wars: The Clone Wars", "Victory
-    // and Death", 9.9));
-    // model.addAttribute("fifth", new Movie("Mr. Robot", "407 Proxy Authentication
+  public String getMovies(Model model) {
+    // commented out -> legacy approach
+    // public ModelAndView getMovies() {
+    model.addAttribute("first", new Movie("Breaking Good", "Ozymandias", 10.0));
+    model.addAttribute("second", new Movie("Attack on Titan", "Hero", 9.9));
+    model.addAttribute("third", new Movie("Attack on Titan", "Perfect Game",
+        9.9));
+    model.addAttribute("fourth", new Movie("Star Wars: The Clone Wars", "Victory and Death", 9.9));
+    model.addAttribute("fifth", new Movie("Mr. Robot", "407 Proxy Authentication Required", 9.9));
+    // Map<String, Movie> model = new HashMap<String, Movie>();
+    // model.put("first", new Movie("Breaking Bad", "Ozymandiaszzz", 10.0));
+    // model.put("second", new Movie("Attack on Titan", "Hero", 9.9));
+    // model.put("third", new Movie("Attack on Titan", "Perfect Game", 9.9));
+    // model.put("fourth", new Movie("Star Wars: The Clone Wars", "Victory and
+    // Death", 9.9));
+    // model.put("fifth", new Movie("Mr. Robot", "407 Proxy Authentication
     // Required", 9.9));
-    Map<String, Movie> model = new HashMap<String, Movie>();
-    model.put("first", new Movie("Breaking Bad", "Ozymandiaszzz", 10.0));
-    model.put("second", new Movie("Attack on Titan", "Hero", 9.9));
-    model.put("third", new Movie("Attack on Titan", "Perfect Game", 9.9));
-    model.put("fourth", new Movie("Star Wars: The Clone Wars", "Victory and Death", 9.9));
-    model.put("fifth", new Movie("Mr. Robot", "407 Proxy Authentication Required", 9.9));
 
-    return new ModelAndView("movies", model);
+    // return new ModelAndView("movies", model);
+    return "movies";
   }
 }
