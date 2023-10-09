a65264b74a669905ef49a8adc655a819e95a5709
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-09 10:12:01 +0800

    Car challenge

---
+++ buchalka-masterclass/Section8OOPPart2/Cars/src/Car.java
@@ -0,0 +1,32 @@
+public class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name){
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getCylinders(){
+        return this.cylinders;
+    }
+    public String startEngine(){
+        return this.getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    public String accelerate(){
+        return this.getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    public String brake(){
+        return this.getClass().getSimpleName() + " -> brake()";
+    }
+}
